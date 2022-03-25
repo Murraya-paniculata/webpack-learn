52eb85c6f8f761256c0021ef3212fbdc5cfb867f
Murraya-paniculata <1395039366@qq.com>
2022-03-25 11:50:26 +0800

    update: 区分环境

--- plugin/webpack.config.js
+++ plugin/webpack.config.js
@@ -1,17 +1,20 @@
 const FileListPlugin = require('./plugins/FileListPlugin');
 var Myplugin = require('./plugins/MyPlugin');
 
-module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    module: {
-        rules: [{
-            test: /\.(png)$/,
-            use: ["./loader/img-loader"]
-        }]
-    },
-    plugins: [
-        new Myplugin(),
-        new FileListPlugin("list")
-    ]
+module.exports = function(env){
+    console.log(env);
+    return {
+        mode: 'development',
+        devtool: 'source-map',
+        module: {
+            rules: [{
+                test: /\.(png)$/,
+                use: ["./loader/img-loader"]
+            }]
+        },
+        plugins: [
+            new Myplugin(),
+            new FileListPlugin("list")
+        ]
+    }
 }
